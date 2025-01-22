9a16acabe3f3773ec50b36ed60865199b4dd8c03
AlexandrTurov <alexandr.turov@mail.ru>
2025-01-23 00:32:04 +0700

    add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,65 @@
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import { Box, Container, IconButton, useColorScheme } from "@mui/material";
+
 import s from "./app.module.sass";
 import Form from "./components/form/form";
 import Scene, { BoxProps } from "./components/scene/scene";
 import { useFetch } from "./hooks/useFetch";
-import { Box } from "./models/models";
+import { Box as IBox } from "./models/models";
 
 function App() {
-  const { data, runFetch, loading, error } = useFetch<BoxProps, Box>("/api/box", (data) => { console.log(data); });
+  const { mode, setMode } = useColorScheme();
+  const { data, runFetch, loading, error } = useFetch<BoxProps, IBox>("/api/box", (data) => { console.log(data); });
+
+  if (!mode) return null;
+
+  const handleChangeMode = function () {
+    if (mode === 'dark') {
+      setMode('light');
+    } else {
+      setMode('dark');
+    }
+  };
 
   if (error) {
     console.error(error);
   }
 
-  const handleSubmit = (box: Box) => {
-    console.log(box);
+  const handleSubmit = (box: IBox) => {
     runFetch(box);
   };
 
   return (
-    <div className={s.container}>
-      <div className={s.menu}>
-        <Form onSubmit={handleSubmit} isLoading={loading} />
-      </div>
-      <div className={s.scene}>
-        <Scene indices={data?.indices} vertices={data?.vertices} />
-      </div>
-    </div>
+    <Container
+      disableGutters
+      sx={[
+        (theme) => ({
+          color: '#fff',
+          backgroundColor: theme.palette.background.paper,
+          height: '100vh'
+        })
+      ]}
+    >
+      <Box component={"header"} position={"fixed"} px={2}>
+        <IconButton onClick={handleChangeMode}>
+          {mode === 'light' && <LightModeOutlinedIcon />}
+          {mode === 'dark' && <DarkModeOutlinedIcon />}
+        </IconButton>
+        <IconButton disabled>
+          <SettingsOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box component={'main'} className={s.container}>
+        <Box className={s.menu}>
+          <Form onSubmit={handleSubmit} isLoading={loading} />
+        </Box>
+        <div className={s.scene}>
+          <Scene indices={data?.indices} vertices={data?.vertices} />
+        </div>
+      </Box>
+    </Container>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,23 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { createTheme, ThemeProvider } from "@mui/material";
+
 import App from "./App.tsx";
 import { makeServer } from "./fakeApi/fakeApi.ts";
 
 makeServer();
 
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
